266fa8a6e47020fdf5619f5b379256c976e107ed
judith atela <judithatela@gmail.com>
2024-08-15 09:11:49 +0300

    python C5 AUG 2024 WK1

---
+++ practice.py
@@ -0,0 +1,19 @@
+# Let's store a favorite number
+Favorite_Number = 77
+print(f"My favorite number is {Favorite_Number}")
+
+# String Variable
+greeting = "Hello, c5 PLP"
+print(greeting)
+
+# Boolean
+# is_python_fun = True
+#print(f"is python fun? {is_python_fun}")\
+
+# Define a Function
+def greet(name):
+    return f"Hello, {name}"
+
+#Apply the function
+print(greet("babyIsrael"))
+print(greet("judi"))
